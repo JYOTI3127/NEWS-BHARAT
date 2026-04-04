480ca7a416170d43c39a4cb043db5b19be354fed
Sheenaa <sheenaas013@gmail.com>
2026-04-04 10:48:26 +0530

    fixed homepage api response

--- Backend/newsapp/serializers.py
+++ Backend/newsapp/serializers.py
@@ -271,14 +271,18 @@ class ArticleHomepageSerializer(serializers.ModelSerializer):
     image_url    = serializers.SerializerMethodField()
     category     = serializers.SerializerMethodField()
     author_name  = serializers.SerializerMethodField()
+    categories   = CategorySerializer(many=True, read_only=True)  
 
     class Meta:
         model  = Article
         fields = [
             'id', 'title', 'slug', 'subtitle',
             'image_url', 'image_alt',
-            'category', 'published_at',
-            'author_name', 'tags', 'is_paid'
+            'category',
+            'categories',              
+            'published_at', 'created_at',
+            'author_name', 'tags', 'is_paid',
+            'selected_subcategories', 
         ]
 
     def get_image_url(self, obj):

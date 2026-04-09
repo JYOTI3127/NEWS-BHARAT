3b8eb3ea5298d7963841a83c892131c6b3e9be5c
JYOTI 3127 <jyotigautam11312711@gmail.com>
2026-04-09 16:55:17 +0530

    Add NotFound page for CI build

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "100px 20px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Yeh page exist nahi karta.</p>
+      <a href="/">Home pe Jao</a>
+    </div>
+  );
+};
+
+export default NotFound;
